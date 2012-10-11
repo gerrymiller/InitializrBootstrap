79461066ca01bddb502a12381cd9e117e6a626e3
Gerry Miller <gerry@museholdings.com>
2012-10-11 15:20:50 -0400

    Initial commit

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace InitializrBootstrap
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.min.css",
+                "~/Content/body.css",
+                "~/Content/bootstrap-responsive.min.css",
+                "~/Content/author.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/libs/modernizr-2.6.1-respond-1.1.0.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/libs/bootstrap.min.js",
+                "~/Scripts/plugins.js",
+                "~/Scripts/main.js"));
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InitializrBootstrap.MvcApplication" Language="C#" %>
