0f48a5184066e89f89e5c1f767926e463bc1ab5d
vcardins <vcardins@gmail.com>
2013-03-17 02:29:45 -0300

    Added MvcSiteMap

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace InitializrBootstrap.Controllers
 {
@@ -12,6 +8,20 @@ namespace InitializrBootstrap.Controllers
         // GET: /Home/
 
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Home/
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // GET: /Home/
+
+        public ActionResult Contact()
         {
             return View();
         }
